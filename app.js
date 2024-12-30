dfd317d778a2bd9beebeb343d5774560ee286d5c
Asaad Ahmed <asaadbinahmed@gmail.com>
2024-12-30 18:34:24 -0500

    Added settings for dot and grid properties

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const GRID_SIZE = 10; // NxN grid
+const DOT_SIZE = 30; // NxN dot (pixels)
+const DOT_GAP = 10; // Gap between dots (pixels)
+const DOT_COLOR = "#000000"; // Color of the dots
 
 document.addEventListener("DOMContentLoaded", function () {
     console.log(`Creating a ${GRID_SIZE}x${GRID_SIZE} grid of dots.`);
@@ -6,8 +9,14 @@ document.addEventListener("DOMContentLoaded", function () {
     const workspace = document.getElementById("workspace");
     const totalDots = GRID_SIZE * GRID_SIZE;
 
+    workspace.style.gridTemplateColumns = `repeat(${GRID_SIZE}, ${DOT_SIZE}px)`;
+    workspace.style.gridTemplateRows = `repeat(${GRID_SIZE}, ${DOT_SIZE}px)`;
+    workspace.style.gap = `${DOT_GAP}px`;
+
     for (let i = 0; i < totalDots; i++) {
-        workspace.appendChild(document.createElement("div"));
+        const dot = document.createElement("div");
+        dot.style.backgroundColor = DOT_COLOR;
+        workspace.appendChild(dot);
     }
 
     console.log("Successfully created grid dots.");
